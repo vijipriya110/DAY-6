3c851395c20b17870ec94ce237830099b4e35ef3
vijipriya110 <124969871+vijipriya110@users.noreply.github.com>
2023-03-01 19:22:58 -0500

    Add files via upload

---
+++ script9.js
@@ -0,0 +1,19 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+    var res=result.filter((ele)=>ele.region==="Asia");
+    console.log(res);
+    var res=result.filter((ele)=>ele.population<200000);
+    console.log(res);
+    var res=result.reduce((a,b)=>a+b.population,0);
+    console.log(res);
+    var res=result.filter((ele)=>ele.currencies.name[0]==="United States Doller");
+    console.log(res);
+
+
+
+
+}
